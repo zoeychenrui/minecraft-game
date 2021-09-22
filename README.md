f49b5a2b76a689bc8c75395d5ee2d13819cd6a36
Zoey Chen <8047902@isd535.org>
2021-09-22 09:46:28 -0500

    added canvas and row of dots (buggy)

--- README.md
+++ README.md
@@ -1,12 +1,2 @@
-[![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-f059dc9a6f8d3a56e377f745f24479a46679e63a5d9fe6f495e02850cd0d8118.svg)](https://classroom.github.com/online_ide?assignment_repo_id=5695895&assignment_repo_type=AssignmentRepo)
-This is a repository you can use to start your HTML/CSS/JavaScript project.
-
-I have included: index.html, styles.css, main.js, and bs.bat - the batch file that will start browser-sync automatically
-
-There is also a .gitignore which will cause GitHub to ignore a bunch of files that shouldn't be stored (like temp files, log files, node libraries, etc)
-
-Make sure you edit *this file* (REAMDE.md), delete all this information and put in your name and a short description of this project.
-
-Then, edit index.html and add your name in the author meta field. 
-
-Finally, commit and push to make sure it's working for you.
+Zoey Chen 
+This is my first game project 
--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,46 @@
-// Put your JavaScript here
+let canvas = document.getElementById("myCanvas");
+let ctx = canvas.getContext("2d");
+
+let rightPressed = false; 
+let leftPressed = false;
+let upPressed = false; 
+let downPressed = false;
+
+let dotRadius = 5; 
+let dotOffsetLeft = 5; 
+let dotOffsetTop = 5;
+let dotWidthCount = 20; 
+
+let wallWidth = 15; 
+let wallHeight = 70; 
+
+let lives = 3;
+let level = 1;
+
+let dots = [];
+
+for (let r=0; r < dotWidthCount; r++) {
+    dots[r] = { x:0, y:0, show: true};
+}
+
+function drawDots1() {
+    for (let r=0; r < dotWidthCount; r++) {
+        if (dots[r].show ==true) {
+            let dotX = (r*(dotRadius+dotPadding)) + dotOffsetLeft; 
+            let dotY = (r*(dotRadius+dotPadding)) + dotOffsetTop; 
+            dots[r].x = dotX;
+            dots[r].y = dotY;
+            ctx.beginPath();
+            ctx.fillStyle = "hsl("+Date.now()*0.07%360+",80%,50%)";
+            ctx.fill();
+            ctx.closePath();
+        }
+    }
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawDots1();
+}
+var start = new Date(); 
+var elapsed = new Date() - start / 100; 

f647b1bc9ecb70185706a5470d81be12ed43260c
Zoey Chen <8047902@isd535.org>
2021-09-23 09:43:29 -0500

    added box  for game

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,18 @@
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
 
+
+
 let rightPressed = false; 
 let leftPressed = false;
 let upPressed = false; 
 let downPressed = false;
 
 let dotRadius = 5; 
-let dotOffsetLeft = 5; 
-let dotOffsetTop = 5;
-let dotWidthCount = 20; 
+let dotOffsetLeft = 310; 
+let dotOffsetTop = 110;
+let dotWidthCount = 30; 
+let dotPadding = 15;
 
 let wallWidth = 15; 
 let wallHeight = 70; 
@@ -25,12 +28,13 @@ for (let r=0; r < dotWidthCount; r++) {
 
 function drawDots1() {
     for (let r=0; r < dotWidthCount; r++) {
-        if (dots[r].show ==true) {
+        if (dots[r].show == true) {
             let dotX = (r*(dotRadius+dotPadding)) + dotOffsetLeft; 
-            let dotY = (r*(dotRadius+dotPadding)) + dotOffsetTop; 
+            let dotY = dotOffsetTop;
             dots[r].x = dotX;
             dots[r].y = dotY;
             ctx.beginPath();
+            ctx.arc(dotX, dotY, dotRadius, 0, Math.PI*2);
             ctx.fillStyle = "hsl("+Date.now()*0.07%360+",80%,50%)";
             ctx.fill();
             ctx.closePath();
@@ -38,9 +42,18 @@ function drawDots1() {
     }
 }
 
+
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawDots1();
+    ctx.beginPath();
+    ctx.lineWidth = 2;
+    ctx.rect(300, 100, 600, 450);
+    ctx.stroke();
 }
+
+
+let interval = setInterval(draw,10);
+
 var start = new Date(); 
 var elapsed = new Date() - start / 100; 

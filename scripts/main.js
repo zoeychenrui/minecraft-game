b36c164b2660981b4feb6e6f9c80a9297745a80c
Zoey Chen <8047902@isd535.org>
2021-09-24 09:06:10 -0500

    perimeter of dots

--- scripts/main.js
+++ scripts/main.js
@@ -9,9 +9,10 @@ let upPressed = false;
 let downPressed = false;
 
 let dotRadius = 5; 
-let dotOffsetLeft = 310; 
-let dotOffsetTop = 110;
-let dotWidthCount = 30; 
+let dotOffsetLeft = 465; 
+let dotOffsetTop = 125;
+let dotWidthCount = 29; 
+let dotHeightCount = 20;
 let dotPadding = 15;
 
 let wallWidth = 15; 
@@ -26,11 +27,11 @@ for (let r=0; r < dotWidthCount; r++) {
     dots[r] = { x:0, y:0, show: true};
 }
 
-function drawDots1() {
+function drawDots1(top) {
     for (let r=0; r < dotWidthCount; r++) {
         if (dots[r].show == true) {
             let dotX = (r*(dotRadius+dotPadding)) + dotOffsetLeft; 
-            let dotY = dotOffsetTop;
+            let dotY = dotOffsetTop + top;
             dots[r].x = dotX;
             dots[r].y = dotY;
             ctx.beginPath();
@@ -42,14 +43,37 @@ function drawDots1() {
     }
 }
 
+function drawDots2(left) {
+    for (let r=0; r < dotHeightCount; r++) {
+        if (dots[r].show == true) {
+            let dotX = dotOffsetLeft + left; 
+            let dotY = (r*(dotRadius+dotPadding)) + dotOffsetTop; 
+            dots[r].x = dotX;
+            dots[r].y = dotY;
+            ctx.beginPath();
+            ctx.arc(dotX, dotY, dotRadius, 0, Math.PI*2);
+            ctx.fillStyle = "hsl("+Date.now()*0.07%360+",80%,50%)";
+            ctx.fill();
+            ctx.closePath();
+        }
+    }
+}
+
+function drawRect() {
+    ctx.beginPath();
+    ctx.lineWidth = 2;
+    ctx.rect(440, 100, 610, 450);
+    ctx.stroke();
+}
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawDots1();
-    ctx.beginPath();
-    ctx.lineWidth = 2;
-    ctx.rect(300, 100, 600, 450);
-    ctx.stroke();
+    drawDots1(0);
+    drawDots1(400);
+    drawRect();
+    drawDots2(0);
+    drawDots2(560);
+
 }
 
 
